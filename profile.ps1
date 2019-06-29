81fb393c819359bd7f9b7506924567aa492dc183
Michael <sheveksmath@gmail.com>
2019-06-29 14:40:39 -0400

    added comments, added Invoke-GitAddCommit

--- profile.ps1
+++ profile.ps1
@@ -1,10 +1,49 @@
 function vi($File) {
+    <#
+    .SYNOPSIS
+        Allows you to open a file using Vim. Assumes you have the Git Bash installed.
+    .EXAMPLE
+        vi .\myFile.ps1
+
+        Opens file in Vim.
+    #>
+
     $File = $File -replace "\\", "/" -replace " ", "/"
     bash -c "vi $File"
 }
 
 function Get-MyModules {
+    <#
+    .SYNOPSIS
+        Shortcut for getting to my PowerShell modules folder.
+    #>
     [Alias("gmm")]
     param()
     Set-Location $HOME\Documents\WindowsPowerShell\Modules\
+}
+
+function Invoke-GitAddCommit {
+    <#
+    .SYNOPSIS
+        Shortcut for adding and committing files using git.
+    .EXAMPLE
+        Invoke-GitAddCommit "initial commit"
+        gac "my commit" .\myFile.ps1
+    #>
+    [Alias("gac")]
+    param(
+        [Parameter(Mandatory = $true, Position = 0)]
+        [string]
+        $Message,
+        [Parameter(Position = 1)]
+        [string]
+        $File
+    )
+    if ($File) {
+        git add $File
+    }
+    else {
+        git add .
+    }
+    git commit -m $Message
 }
